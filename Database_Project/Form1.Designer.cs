6951b89e901821b71d42836d5d3ecb03489783b7
TowerOne <TowerOne@TOWERONE-PC>
2016-10-06 21:43:36 -0500

    test commit from desktop

--- Database_Project/Form1.Designer.cs
+++ Database_Project/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(773, 341);
+            this.ClientSize = new System.Drawing.Size(791, 353);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);

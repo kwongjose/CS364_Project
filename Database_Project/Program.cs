cc582d15473d20d207cb7a6783c8e7ce64fdc95e
Waleed Mahmoud <wmahmoud@uwlax.edu>
2016-11-14 13:56:09 -0600

    [WMM] Commented out testing code

--- Database_Project/Program.cs
+++ Database_Project/Program.cs
@@ -12,25 +12,25 @@ namespace Database_Project {
         [STAThread]
         static void Main() {
             //Below is testing code
-            ConnectionClass conTest = new ConnectionClass();
-            conTest.createStreamingTable();
+            //ConnectionClass conTest = new ConnectionClass();
+            //conTest.createStreamingTable();
             //conTest.addMovieWithTitle( "13 Assassins" );
-            conTest.addMovieWithTitle( "The Revenant" );
-            conTest.addMovieWithTitle( "Naked Gun" );
-            conTest.addMovieWithTitle( "Cast Away" );
-            conTest.addMovieWithTitle( "Act Of Valor" );
-            conTest.addMovieWithTitle( "10 Cloverfield Lane" );
-            conTest.addMovieWithTitle( "Monty Python and the Holy Grail" );
-            conTest.addMovieWithTitle( "Saving Private Ryan" );
-            conTest.addMovieWithTitle( "Airplane!" );
-            conTest.addMovieWithTitle( "Space Jam" );
-            conTest.addMovieWithTitle( "Taken" );
-            conTest.addMovieWithTitle( "A Walk to Remember" );
-            conTest.addMovieWithTitle( "Encino Man" );
-            conTest.addMovieWithTitle( "Big trouble in little china" );
-            conTest.addMovieWithTitle( "Good Burger" );
-            conTest.addMovieWithTitle("The cobbler" );
-            conTest.close();
+            //conTest.addMovieWithTitle( "The Revenant" );
+            //conTest.addMovieWithTitle( "Naked Gun" );
+            //conTest.addMovieWithTitle( "Cast Away" );
+            //conTest.addMovieWithTitle( "Act Of Valor" );
+            //conTest.addMovieWithTitle( "10 Cloverfield Lane" );
+            //conTest.addMovieWithTitle( "Monty Python and the Holy Grail" );
+            //conTest.addMovieWithTitle( "Saving Private Ryan" );
+            //conTest.addMovieWithTitle( "Airplane!" );
+            //conTest.addMovieWithTitle( "Space Jam" );
+            //conTest.addMovieWithTitle( "Taken" );
+            //conTest.addMovieWithTitle( "A Walk to Remember" );
+            //conTest.addMovieWithTitle( "Encino Man" );
+            //conTest.addMovieWithTitle( "Big trouble in little china" );
+            //conTest.addMovieWithTitle( "Good Burger" );
+            //conTest.addMovieWithTitle("The cobbler" );
+            //conTest.close();
             //Above is testing code
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault( false );
